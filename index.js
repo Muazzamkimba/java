f6da025c229a44fa3809b9c42c4931f862c0fe00
Engr Muazzam Kimba <ibrahimkimba75@gmail.com>
2023-06-19 11:50:36 +0000

    index.js

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
      return `It's a tie! you both picked ${playerSelection}`;
-  } else if (playerSefunction playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-     return `It's a tie! you both picked ${playerSelection}`;
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
      return "You win! Rock beats Scissors";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
@@ -18,25 +15,8 @@ function playRound(playerSelection, computerSelection) {
   } else {
      return `You lose! ${computerSelection} beats ${playerSelection}`;
   }
-function playRound(playerSelection, computerSelection) {
-lection === "rock" && computerSelection === "scissors") {
-     return "You win! Rock beats Scissors";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-     return "You win! Paper beats Rock";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-     return "You win! Scissors beats Paper";
-  } else {
-     return `You lose! ${computerSelection} beats ${playerSelection}`;
-  }
-function playRound(playerSelection, computerSelection) {
-  // your code here!
-} 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
 }
+
 const playerSelection = prompt("Start the game by picking among 'Rock, Paper, Scissors'").toLowerCase();
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection))
-
-
