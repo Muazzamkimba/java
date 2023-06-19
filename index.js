4d3d5f4f0ad0dee4edc156ed0e933a73fead4c91
Engr Muazzam Kimba <ibrahimkimba75@gmail.com>
2023-06-19 15:39:47 +0000

    index.js

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection))
 */
 
+/*
 let sign = prompt("What's your sign?");
 
 if (sign.toLowerCase() === "scorpio") {
@@ -34,3 +35,8 @@ sign = window.prompt(); // open the blank prompt window
 sign = prompt(); //  open the blank prompt window
 sign = window.prompt("Are you feeling lucky"); // open the window with Text "Are you feeling lucky"
 sign = window.prompt("Are you feeling lucky", "sure"); // open the window with Text "Are you feeling lucky" and default value "sure"
+*/
+
+
+prompt(message)
+
