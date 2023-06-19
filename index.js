40518ea99fc44ca2fbd29643d1792ac51a49f521
Engr Muazzam Kimba <ibrahimkimba75@gmail.com>
2023-06-19 16:22:31 +0000

    index.js

--- index.js
+++ index.js
@@ -46,4 +46,16 @@ if (sign === "scorpio") {
 }
 
 let signn = prompt("are you feeling lucky?");
-let signnn = prompt("are you feeling lucky? sure");
+let signnn = prompt("are you feeling lucky?", "sure");
+/*let sign = prompt("What's your sign?");
+
+if (sign.toLowerCase() === "scorpio") {
+  alert("Wow! I'm a Scorpio too!");
+}
+
+// there are many ways to use the prompt feature
+sign = window.prompt(); // open the blank prompt window
+sign = prompt(); //  open the blank prompt window
+sign = window.prompt("Are you feeling lucky"); // open the window with Text "Are you feeling lucky"
+sign = window.prompt("Are you feeling lucky", "sure"); // open the window with Text "Are you feeling lucky" and default value "sure"
+*/
