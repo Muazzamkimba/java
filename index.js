d2f365845cb551f06eaee315d474dc7d2bbe03b3
Engr Muazzam Kimba <ibrahimkimba75@gmail.com>
2023-06-19 11:54:27 +0000

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 function computerPlay() {
   const pick = ['rock', 'paper', 'scissors'];
   return pick[Math.floor(Math.random() * pick.length)];
@@ -20,3 +21,16 @@ function playRound(playerSelection, computerSelection) {
 const playerSelection = prompt("Start the game by picking among 'Rock, Paper, Scissors'").toLowerCase();
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection))
+*/
+
+let sign = prompt("What's your sign?");
+
+if (sign.toLowerCase() === "scorpio") {
+  alert("Wow! I'm a Scorpio too!");
+}
+
+// there are many ways to use the prompt feature
+sign = window.prompt(); // open the blank prompt window
+sign = prompt(); //  open the blank prompt window
+sign = window.prompt("Are you feeling lucky"); // open the window with Text "Are you feeling lucky"
+sign = window.prompt("Are you feeling lucky", "sure"); // open the window with Text "Are you feeling lucky" and default value "sure"
