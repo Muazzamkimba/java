b966c61304db18aed771d2e28e1dfb555cae3f71
Engr Muazzam Kimba <ibrahimkimba75@gmail.com>
2023-06-20 19:27:14 +0000

    index.js

--- index.js
+++ index.js
@@ -62,3 +62,13 @@ sign = prompt(); //  open the blank prompt window
 sign = window.prompt("Are you feeling lucky"); // open the window with Text "Are you feeling lucky"
 sign = window.prompt("Are you feeling lucky", "sure"); // open the window with Text "Are you feeling lucky" and default value "sure"
 */
+
+const sumArray = function(array) {
+   let sum = 0;
+   array.forEach(function(number) {
+     sum += number;
+   });
+   return sum;
+ };
+ 
+ sumArray([2, 2, 2]);
