75dda209c4bc2c3583128486b609693dbf8ed557
Engr Muazzam Kimba <ibrahimkimba75@gmail.com>
2023-06-22 08:58:20 +0000

    array.js

--- array.js
+++ array.js
@@ -1,7 +1,15 @@
+/*
 let question = prompt("do you agreed?")
 if(question === "yes"){
     alert('Approved');
 }
 else{
     alert('Disapproved');
+}
+*/
+
+let question = prompt("what do you eat?");
+if (question === food);
+    let food = ['beans', 'rice', 'yam'];
+   {console.log('food')
 }
