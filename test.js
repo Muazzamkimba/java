fcae2c6563f4c55a8bd6cebdd6a2f28aa780673b
Engr Muazzam Kimba <ibrahimkimba75@gmail.com>
2023-07-08 10:16:26 +0000

    test.js

--- test.js
+++ test.js
@@ -1,11 +1,2 @@
-//let = stock[0] = 'laptop';
-//let = stock[1] = 'battery';
-//let = stock[2] = 'fan';
-//let = stock[3] = 'printer';
-stock = ['laptop', 'battery', 'fan', 'printer'];
-    stock[4] = 'radio'
-    stock[5] = 'handfan'
-    stock[6] = 'macbook'
-    stock[1] = 'bez'
-    console.log(stock);
+
 
