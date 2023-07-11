6b7bad649492a90d7be4d6a7bb7865d70c1eae9d
Engr Muazzam Kimba <ibrahimkimba75@gmail.com>
2023-07-11 11:27:30 +0000

    test.js

--- test.js
+++ test.js
@@ -66,7 +66,7 @@ const citrus = fruits1.slice(1);*/
 //const citrus = fruits.slice(3);
 //console.log(citrus)
 
-const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
-const citrus = fruits.slice(1,3);
+const fruits = ["Banana", "Orange", 'sos', "Lemon", 'mine', "Apple", "Mango"];
+const citrus = fruits.slice(2,3);
 console.log(citrus)
 
