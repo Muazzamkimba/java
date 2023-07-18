86fe7515e126dcbee4ae3fc1c0ff7b29b111d683
Engr Muazzam Kimba <ibrahimkimba75@gmail.com>
2023-07-18 08:50:49 +0000

    loops.js

--- loops.js
+++ loops.js
@@ -9,3 +9,16 @@ function toUpper(string){
     const cats =['leopard'. 'serla', 'jaguar', 'tiger', 'lion'];
     const upperCats = cats.map(toUpper);
     console.log(upperCats); 
+
+
+    function toUpper(string) {
+        return string.toUpperCase();
+      }
+      
+      const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+      
+      const upperCats = cats.map(toUpper);
+      
+      console.log(upperCats);
+      // [ "LEOPARD", "SERVAL", "JAGUAR", "TIGER", "CARACAL", "LION" ]
+      
