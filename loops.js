e8ecc6a478fa6bbeafdd71266cf2c50d1182e791
Engr Muazzam Kimba <ibrahimkimba75@gmail.com>
2023-07-18 08:44:35 +0000

    loops.js

--- loops.js
+++ loops.js
@@ -2,3 +2,9 @@ const cats = ['leapard', 'servant', 'jaguar', 'tiger', 'caracai', 'lion'];
 for(const cat of cats){
     console.log(cat);
 }
+
+function toUpper(string){
+    return string.toUpper();
+}
+    const cats =['leopard'. 'serla', 'jaguar', 'tiger', 'lion',]
+    
