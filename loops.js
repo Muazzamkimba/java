1250706a257ab286aa38d629ab09b82ea1c09669
Engr Muazzam Kimba <ibrahimkimba75@gmail.com>
2023-08-07 18:03:58 +0000

    loops.js

--- loops.js
+++ loops.js
@@ -4,22 +4,6 @@ for(const cat of cats){
 }*/
 
 
-const para = document.querySelector("p");
-const input = document.querySelector("input");
-const btn = document.querySelector("button");
 
-btn.addEventListener("click", () => {
-  para.textContent = "Output: ";
-  const num = input.value;
-  input.value = "";
-  input.focus();
-  for (let i = 1; i <= num; i++) {
-    let sqRoot = Math.sqrt(i);
-    if (Math.floor(sqRoot) !== sqRoot) {
-      continue;
-    }
-    para.textContent += `${i} `;
-  }
-});
 
 
