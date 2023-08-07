ba2b1726235db10535bf96894264e7e5f19099eb
Engr Muazzam Kimba <ibrahimkimba75@gmail.com>
2023-08-07 18:18:15 +0000

    loops.js

--- loops.js
+++ loops.js
@@ -4,6 +4,25 @@ for(const cat of cats){
 }*/
 
 
+const cats = ["Pete", "Biggles", "Jasmine"];
+
+let myFavoriteCats = "My cats are called ";
+
+let i = 0;
+
+while (i < cats.length) {
+  if (i === cats.length - 1) {
+    myFavoriteCats += `and ${cats[i]}.`;
+  } else {
+    myFavoriteCats += `${cats[i]}, `;
+  }
+
+  i++;
+}
 
 
 
+console.log(myFavoriteCats); // "My cats are called Pete, Biggles, and Jasmine."
+
+
+console.log(boy)
